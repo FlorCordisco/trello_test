1a05d2293113d0946fabc701d5554ebdeb5a3c7d
Florencia <florcordisco@gmail.com>
2019-09-23 13:31:14 -0300

    Api inmplementacion completa

--- features/api/api_test.rb
+++ features/api/api_test.rb
@@ -1,7 +1,6 @@
 require 'uri'
 require 'net/http'
 require 'json'
-require 'pry'
 
 class ApiTest
     
@@ -20,9 +19,59 @@ class ApiTest
         request["content-length"] = ''
         request["Connection"] = 'keep-alive'
         request["cache-control"] = 'no-cache'
-
         response = http.request(request)
-        json = JSON.parse(response.read_body)
+        JSON.parse(response.read_body)
+    end
+
+    def create_list(name,id_board,api_key,token)
+        url = URI(@@url + 'lists/?name=' + name + '&idBoard=' + id_board + '&key=' + api_key + '&token=' + token)
+        http = Net::HTTP.new(url.host, url.port)
+        request = Net::HTTP::Post.new(url)
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        http.use_ssl = true
+        request["Accept"] = '*/*'
+        request["Cache-Control"] = 'no-cache'
+        request["Host"] = 'api.trello.com'
+        request["cookie"] = 'dsc=e55f116952c598b822e2212cb2842da48a05f97145642aa1237e42f497ce97d4'
+        request["content-length"] = ''
+        request["Connection"] = 'keep-alive'
+        request["cache-control"] = 'no-cache'
+        response = http.request(request)
+        JSON.parse(response.read_body)
+    end
+    
+    def create_card(name,id_list,api_key,token)
+        url = URI(@@url + 'cards?name=' + name + '&idList=' + id_list + '&keepFromSource=all&key=' + api_key + '&token=' + token)
+        http = Net::HTTP.new(url.host, url.port)
+        request = Net::HTTP::Post.new(url)
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        http.use_ssl = true
+        request["Accept"] = '*/*'
+        request["Cache-Control"] = 'no-cache'
+        request["Host"] = 'api.trello.com'
+        request["cookie"] = 'dsc=e55f116952c598b822e2212cb2842da48a05f97145642aa1237e42f497ce97d4'
+        request["content-length"] = ''
+        request["Connection"] = 'keep-alive'
+        request["cache-control"] = 'no-cache'
+        response = http.request(request)
+        JSON.parse(response.read_body)
+    end
+
+    def get_card(id_card,api_key,token)
+        url = URI(@@url + 'cards/' + id_card +'?key=' + api_key + '&token=' + token)
+        http = Net::HTTP.new(url.host, url.port)
+        request = Net::HTTP::Get.new(url)
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        http.use_ssl = true
+        request["Accept"] = '*/*'
+        request["Cache-Control"] = 'no-cache'
+        request["Host"] = 'api.trello.com'
+        request["cookie"] = 'dsc=e55f116952c598b822e2212cb2842da48a05f97145642aa1237e42f497ce97d4'
+        request["content-length"] = ''
+        request["Connection"] = 'keep-alive'
+        request["cache-control"] = 'no-cache'
+        response = http.request(request)
+        JSON.parse(response.read_body)
     end
 
 end
--- features/step_definitions/api_steps.rb
+++ features/step_definitions/api_steps.rb
@@ -7,16 +7,22 @@ require 'rspec'
     end
 
     When("I create a new board") do
-       json = @api.create_board('Tablero-API',@api_key,@token)
-       @board_id = json['id'] 
-       puts @board_id
+       @json = @api.create_board('Tablero-API',@api_key,@token)
     end
 
     And("I get the board id") do
+       @board_id = @json['id'] 
+       puts @board_id
     end
 
     And("I create a card in the first list of the board") do
+        @json = @api.create_list('List-Api',@board_id,@api_key,@token)
+        @list_id = @json['id']
+        @json_card = @api.create_card('Card-Api',@list_id,@api_key,@token)
     end
 
     Then("I should see the card is successfully created") do
+        @card_id = @json_card['id']
+        json_card = @api.get_card(@card_id,@api_key,@token)
+        (json_card['name'] == 'Card-Api').should be true
     end
